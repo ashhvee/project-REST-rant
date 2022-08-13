8ab8c92322f84b42359b62d4cb13210fc21fec03
Ashley Vieira <ash.adr24212@gmail.com>
2022-08-13 10:51:24 -0400

    add 404 image

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src = "/images/404.jpg" alt = "Sunset on water" />
+                </div>
+                <div>
+                    Photo by<a href="https://unsplash.com/@coopery?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mohamed Nohassi</a> on <a href="https://unsplash.com/s/photos/free?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
             </main>
         </Def>
     )
