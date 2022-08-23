80b76da528aa9296c5368c7de6b12ca8e7a1f206
Ashley Vieira <ash.adr24212@gmail.com>
2022-08-22 21:35:51 -0400

    navigate from index to show by clicking link

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,83 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
+const db = require('../models')
 
+
+// Index
 router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
+})
+
+
+// Post
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+
+// 
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+
+// Show
+router.get('/:id', (req, res) => {
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render('places/show', { place })
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
+})
+
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+
+module.exports = router
+
+
+
+
+
+
+
+/* router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
 
@@ -96,7 +172,7 @@ router.delete('/:id', (req, res) => {
         places.splice(id, 1)
         res.redirect('/places')
     }
-})
+}) */
 
 
 // Update
