5d41f457c461e7dff07911cbfcc52d9b93f0c2bc
Ashley Vieira <ash.adr24212@gmail.com>
2022-08-27 12:12:35 -0400

    comment form

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,54 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className = 'border'>
+                <form action = './controllers/places.js' method = 'POST' />
+                    <label htmlFor = 'author'>Author</label>
+                        <input
+                            type = 'text'
+                            name = 'author'
+                            id = 'author'
+                        />
+                    <label htmlFor = 'content'>Content</label>
+                        <input
+                            type = 'text'
+                            name = 'content'
+                            id = 'content'
+                        />
+                    <label htmlFor = 'rant'>Rant</label>
+                        <input
+                            type = 'checkbox'
+                            name = 'rant'
+                            id = 'rant'
+                        />
+                    <label htmlFor = 'starRating'>Star Rating</label>
+                        <input
+                            type = 'number'
+                            name = 'starRating'
+                            id = 'starRating'
+                            min = '0'
+                            max = '5'
+                            step = '0.5'
+                        />
+                    <input type = 'submit' />
+                    <h2 className = 'rant'>{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -17,8 +65,9 @@ function show (data) {
                 <h4>Serving {data.place.cuisines}</h4>
                 <br />
                 </div>
-                <h4>Comments</h4>
-                    <p>No comments yet!</p>
+                <h2>Comments</h2>
+                    {comments}
+                    {/* <p>No comments yet!</p> */}
                 </div>
                 <a href = {`/places/${data.id}/edit`} className = 'btn btn-warning'>
                     Edit
